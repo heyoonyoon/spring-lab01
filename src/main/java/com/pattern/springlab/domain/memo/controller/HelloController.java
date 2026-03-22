ccbe30e021ef7454f7b46094c4b2a5c5a7bf92a5
_yoonyoon_1 <heeyoon1302@naver.com>
2026-03-22 11:47:35 +0900

    refactor: 도메인형 패키지 구조로 변경

--- src/main/java/com/pattern/springlab/domain/memo/controller/HelloController.java
+++ src/main/java/com/pattern/springlab/domain/memo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.pattern.springlab.controller;
+package com.pattern.springlab.domain.memo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
